757f0c59f92441032977327358b3b8186b6cf92e
MarwaAshraf1812 <marashraf090@gmail.com>
2024-12-10 20:57:57 +0200

    first next app

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -8,7 +8,10 @@ interface SinglePageProps {
   };
 }
 
-const SingleArticlePage = async ({ params: { id } }: SinglePageProps) => {
+const SingleArticlePage = async ({ params }: SinglePageProps) => {
+  const { id } = params;
+
+  // Fetch article data
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${id}`
   );
@@ -16,6 +19,7 @@ const SingleArticlePage = async ({ params: { id } }: SinglePageProps) => {
     throw new Error("Failed to fetch article");
   }
   const article: Article = await response.json();
+
   return (
     <section className="container p-5 m-auto w-full px-5 pt-8 md:w-3/4 fixed_height">
       <div className="shadow-lg p-5">
@@ -26,7 +30,7 @@ const SingleArticlePage = async ({ params: { id } }: SinglePageProps) => {
             <span>{article.userId}</span>
           </div>
           <div>
-            <span className="font-bold">date:</span>
+            <span className="font-bold">Date:</span>
             <span>1/1/2023</span>
           </div>
           <p className="mt-5 text-gray-600">{article.body}</p>
