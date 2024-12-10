025c6b886ed178c707abf03980152eddd636067e
MarwaAshraf1812 <marashraf090@gmail.com>
2024-12-10 20:54:15 +0200

    first next app

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,11 @@ function page() {
       <div className="container mx-auto px-6 lg:px-20 text-center">
         <h1 className="text-4xl font-bold text-gray-800 mb-6">About Us</h1>
         <p className="text-lg text-gray-600 mb-8 leading-relaxed">
-          Welcome to <span className="text-blue-500">Web Hoisting</span>, where innovation meets reliability in the world of cloud hosting. Since our inception, we have been committed to delivering cutting-edge solutions that empower businesses of all sizes to scale, thrive, and achieve unparalleled online success.
+          Welcome to <span className="text-blue-500">Web Hoisting</span>, where
+          innovation meets reliability in the world of cloud hosting. Since our
+          inception, we have been committed to delivering cutting-edge solutions
+          that empower businesses of all sizes to scale, thrive, and achieve
+          unparalleled online success.
         </p>
         <div className="text-left bg-white shadow-md rounded-lg p-8">
           <h2 className="text-2xl font-semibold text-gray-800 mb-4">
@@ -21,10 +25,13 @@ function page() {
               protocols.
             </li>
             <li>
-              <strong className="text-orange-500">Scalability:</strong> Flexible hosting plans designed to grow with your business.
+              <strong className="text-orange-500">Scalability:</strong> Flexible
+              hosting plans designed to grow with your business.
             </li>
             <li>
-              <strong className="text-teal-500">Eco-Friendly Practices:</strong> Committed to reducing our environmental footprint with green hosting initiatives.
+              <strong className="text-teal-500">Eco-Friendly Practices:</strong>{" "}
+              Committed to reducing our environmental footprint with green
+              hosting initiatives.
             </li>
           </ul>
         </div>
@@ -33,7 +40,8 @@ function page() {
             Join Us Today!
           </h2>
           <p className="text-lg text-gray-600 mb-6">
-            Experience the future of web hosting. Together, let's build a better, faster, and more secure web!
+            Experience the future of web hosting. Together, let's build a
+            better, faster, and more secure web!
           </p>
           <button className="bg-blue-500 text-white px-6 py-3 rounded-lg shadow-md hover:bg-blue-600 transition">
             Get Started
@@ -41,7 +49,7 @@ function page() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
